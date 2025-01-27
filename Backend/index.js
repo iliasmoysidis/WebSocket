0f4e8b70f685763770ccfabae807a1fab09fca10
iliamous92 <iliamous@iti.gr>
2025-01-27 14:10:11 +0200

    Simplify code

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,8 @@
 const PORT = 3000;
 const CONNECTED_USERS = [];
+const LOGGED_USERS = new Map();
+
+const users = { public: [], authenticated: new Map() };
 
 const express = require("express");
 const app = express();
@@ -11,19 +14,75 @@ const io = require("socket.io")(server, {
   },
 });
 
+function addPublicUser(socket) {
+  console.log(`User ${socket.id} connected`);
+  users.public.push(socket);
+}
+
+function removePublicUser(socket) {
+  const index = users.public.findIndex((u) => u.id === socket.id);
+  if (index !== -1) {
+    users.public.splice(index, 1);
+    console.log(`User ${socket.id} disconnected`);
+  }
+}
+
+function addAuthenticatedUser(socket, email) {
+  users.authenticated.set(socket, email);
+  console.log(`User with email ${email} has logged in`);
+}
+
+function removeAuthenticatedUser(userEmail) {
+  for (let [socket, email] of LOGGED_USERS.entries()) {
+    if (email === userEmail) {
+      users.authenticated.delete(socket);
+      console.log(`User with email ${email} has logged out`);
+      break;
+    }
+  }
+}
+
 server.listen(PORT, () => {
   console.log(`Server is now listening on PORT ${PORT}`);
 });
 
+app.post("/callback", (req, res) => {
+  const { userEmail, url } = req.body;
+
+  let userFound = false;
+  for (let [socket, email] of users.authenticated.entries()) {
+    if (userEmail === email) {
+      const targetSocket = io.sockets.sockets.get(socket.id);
+      targetSocket.emit("url", url);
+      userFound = true;
+      break;
+    }
+  }
+
+  if (userFound) {
+    return res
+      .status(200)
+      .json({ message: `Event sent to user with email ${userEmail}` });
+  } else {
+    return res
+      .status(404)
+      .json({ message: `User with email ${userEmail} not found` });
+  }
+});
+
 io.on("connect", (socket) => {
-  console.log(`User ${socket.id} connected`);
-  CONNECTED_USERS.push(socket);
+  addPublicUser(socket);
 
   socket.on("disconnect", () => {
-    const index = CONNECTED_USERS.findIndex((u) => u.id === socket.id);
-    if (index !== -1) {
-      CONNECTED_USERS.splice(index, 1);
-      console.log(`User ${socket.id} disconnected`);
-    }
+    removePublicUser(socket);
+  });
+
+  socket.on("login", (email) => {
+    addAuthenticatedUser(socket, email);
+  });
+
+  socket.on("logout", (email) => {
+    removeAuthenticatedUser(email);
+    removePublicUser(socket);
   });
 });
